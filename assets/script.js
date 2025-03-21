ac45478bf54500318804a1a321b4335e1550b35b
Eren Erdoğan <allahsizpisagor@gmail.com>
2025-03-21 17:18:25 +0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("Welcome to Κοdyefyra!");
+  const languageSelector = document.getElementById("language");
+  const elements = document.querySelectorAll("[data-lang]");
+
+  const savedLanguage = localStorage.getItem("language") || "en";
+  languageSelector.value = savedLanguage;
+  changeLanguage(savedLanguage);
+
+  languageSelector.addEventListener("change", function () {
+    const selectedLanguage = this.value;
+    localStorage.setItem("language", selectedLanguage);
+    changeLanguage(selectedLanguage);
+  });
+
+  function changeLanguage(lang) {
+    elements.forEach((element) => {
+      element.innerHTML = element.dataset[lang];
+    });
+  }
 });
